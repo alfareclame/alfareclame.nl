d3e6e2dc448f2d28c4544242563bc4089cf26a18
fleettrackholland-netizen <fleettrackholland@gmail.com>
2026-06-01 12:08:59 +0200

    feat: add dynamic copyrightNotice to ImageObject JSON-LD
    Add copyrightNotice field to all 10 licensed ImageObject blocks
    (those carrying creditText/copyrightHolder). Year auto-updates each
    build via new site.year global (new Date().getFullYear()).

--- _data/site.js
+++ _data/site.js
@@ -1,3 +1,5 @@
 module.exports = {
-  maps_url: "https://maps.app.goo.gl/Kb5gAibeohV3Shmz7"
+  maps_url: "https://maps.app.goo.gl/Kb5gAibeohV3Shmz7",
+  // Auto-updates each build; used in JSON-LD ImageObject copyrightNotice
+  year: new Date().getFullYear()
 };
